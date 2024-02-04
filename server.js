d55220cd529067609a7eab25954646f4690f57c4
Ryan Hayes <rrh5210@psu.edu>
2024-02-04 14:41:25 -0500

    Updated gitignore

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://ryanhayes0822:<password>@test.vigi5ee.mongodb.net/?retryWrites=true&w=majority";
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-mongoose.connect("mongodb://localhost:27017/my-mern-app", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
